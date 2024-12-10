6d9625efec58b4ab8fc905f16f20ba51f70a2be9
Quan ZHOU <quan@CoreMatter.local>
2024-12-10 20:01:25 +0800

    feat: day6

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -51,7 +51,7 @@ fn solve(input: &str) -> (usize, usize) {
     let (mut start_i, mut start_j): (usize, usize) = (0, 0);
     let (mut di, mut dj): (isize, isize) = (-1, 0);
     let mut visited = HashSet::new();
-
+    
     for (i, j) in (0..m).flat_map(|i| (0..n).map(move |j| (i, j))) {
         if matrix[i][j] == '^' {
             (start_i, start_j) = (i, j);
@@ -59,13 +59,13 @@ fn solve(input: &str) -> (usize, usize) {
     }
 
     let (mut pos_i, mut pos_j): (usize, usize) = (start_i, start_j);
+    visited.insert((pos_i, pos_j));
     while let (Some(i), Some(j)) =
         (pos_i.checked_add_signed(di), pos_j.checked_add_signed(dj))
     {
         if i == m || j == n {
             break;
         }
-        visited.insert((pos_i, pos_j));
         match matrix[i][j] {
             '#' => {
                 (di, dj) = match (di, dj) {
@@ -78,6 +78,7 @@ fn solve(input: &str) -> (usize, usize) {
             }
             '.' | '^' => {
                 (pos_i, pos_j) = (i, j);
+                visited.insert((pos_i, pos_j));
             }
             _ => panic!("disco!"),
         }
@@ -92,8 +93,7 @@ fn solve(input: &str) -> (usize, usize) {
             is_loop(matrix_clone, start_i, start_j, -1, 0)
         })
         .collect();
-
-    (visited.len() + 1, obs.len() + 1)
+    (visited.len(), obs.len())
 }
 
 #[cfg(test)]
